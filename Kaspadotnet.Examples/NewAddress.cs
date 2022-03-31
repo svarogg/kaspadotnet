072c9562a8cbd17fb59fa722d270889268864018
Mike Zak <feanorr@gmail.com>
2022-04-01 01:51:42 +0300

    Add NewAddress example

---
+++ Kaspadotnet.Examples/NewAddress.cs
@@ -0,0 +1,17 @@
+using Kaspadontnet.Clients;
+using Kaspawalletd;
+
+namespace Kaspadotnet.Examples;
+
+public partial class RPCExamples
+{
+    public async Task NewAddressExample()
+    {
+        using var kaspawalletClient = new KaspawalletdClient();
+        var newAddressRequest = new NewAddressRequest();
+        var newAddressResponse = await kaspawalletClient.Client.NewAddressAsync(newAddressRequest);
+        
+        Console.WriteLine("New Address:");
+        Console.WriteLine(newAddressResponse.Address);
+    }
+}
